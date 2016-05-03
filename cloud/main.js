4c275cdcfac36dc7bfe9f0b498344e885c0ef329
Adriel Café <me@adriel.cafe>
2016-05-03 13:33:00 -0300

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -4,15 +4,13 @@ var wordTranslationClass = Parse.Object.extend("AlienWordTranslation");
 // AlienWord
 Parse.Cloud.beforeSave("AlienWord", function(req, res) {
 	var word = req.object;
+	console.log("BEFORESAVE1: " + word);
+	console.log("BEFORESAVE2: " + word.get("users")._op);
 	word.set("word", word.get("word").toUpperCase())
 	if(willAddRelation(word, "users")){
 		word.increment("usersCount");
-	}
-	res.success();
-});
-Parse.Cloud.beforeDelete("AlienWord", function(req, res) {
-	var word = req.object;
-	if(willRemoveRelation(word, "users")){
+	} else if(willRemoveRelation(word, "users")){
+		console.log("DECREMENT");
 		word.decrement("usersCount");
 	}
 	res.success();
@@ -25,12 +23,7 @@ Parse.Cloud.beforeSave("AlienWordTranslation", function(req, res) {
 	wordTranslation.set("language", wordTranslation.get("language").toLowerCase())
 	if(willAddRelation(wordTranslation, "users")){
 		wordTranslation.increment("usersCount");
-	}
-	res.success();
-});
-Parse.Cloud.beforeDelete("AlienWordTranslation", function(req, res) {
-	var wordTranslation = req.object;
-	if(willRemoveRelation(wordTranslation, "users")){
+	} else if(willRemoveRelation(wordTranslation, "users")){
 		wordTranslation.decrement("usersCount");
 	}
 	res.success();
