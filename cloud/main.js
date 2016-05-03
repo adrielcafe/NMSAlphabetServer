33dde1a36590d67f1fb279461c2fa12996309bf3
Adriel Café <me@adriel.cafe>
2016-05-03 11:57:43 -0300

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,18 +1,7 @@
 var wordClass = Parse.Object.extend("AlienWord");
 var wordTranslationClass = Parse.Object.extend("AlienWordTranslation");
 
-String.prototype.contains = function(it) { return this.indexOf(it) != -1; };
-
-function willAddRelation(obj, relationName){
-	var jsonSnippet = '"' + relationName + '":{"__op":"AddRelation"';
-	return JSON.stringify(obj).contains(jsonSnippet);
-}
-
-function willRemoveRelation(obj, relationName){
-	var jsonSnippet = '"' + relationName + '":{"__op":"RemoveRelation"';
-	return JSON.stringify(obj).contains(jsonSnippet);
-}
-
+// AlienWord
 Parse.Cloud.beforeSave("AlienWord", function(req, res) {
 	var word = req.object;
 	if(willAddRelation(word, "users")){
@@ -20,7 +9,6 @@ Parse.Cloud.beforeSave("AlienWord", function(req, res) {
 	}
 	res.success();
 });
-
 Parse.Cloud.beforeDelete("AlienWord", function(req, res) {
 	var word = req.object;
 	if(willRemoveRelation(word, "users")){
@@ -28,3 +16,31 @@ Parse.Cloud.beforeDelete("AlienWord", function(req, res) {
 	}
 	res.success();
 });
+
+// AlienWordTranslation
+Parse.Cloud.beforeSave("AlienWordTranslation", function(req, res) {
+	var wordTranslation = req.object;
+	if(willAddRelation(wordTranslation, "users")){
+		wordTranslation.increment("usersCount");
+	}
+	res.success();
+});
+Parse.Cloud.beforeDelete("AlienWordTranslation", function(req, res) {
+	var wordTranslation = req.object;
+	if(willRemoveRelation(wordTranslation, "users")){
+		wordTranslation.decrement("usersCount");
+	}
+	res.success();
+});
+
+// Util
+function willAddRelation(obj, relationName){
+	var jsonSnippet = '"' + relationName + '":{"__op":"AddRelation"';
+	return JSON.stringify(obj).contains(jsonSnippet);
+}
+function willRemoveRelation(obj, relationName){
+	var jsonSnippet = '"' + relationName + '":{"__op":"RemoveRelation"';
+	return JSON.stringify(obj).contains(jsonSnippet);
+}
+
+String.prototype.contains = function(it) { return this.indexOf(it) != -1; };
