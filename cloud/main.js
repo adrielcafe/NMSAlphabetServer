ff2ea4053ab84eef1c7f6549350e36aaa966ddae
Adriel Café <me@adriel.cafe>
2016-05-03 11:55:17 -0300

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -3,25 +3,28 @@ var wordTranslationClass = Parse.Object.extend("AlienWordTranslation");
 
 String.prototype.contains = function(it) { return this.indexOf(it) != -1; };
 
-function printError(tag, error){
-	if(error){
-		console.error("ERROR " + tag + ":" + error["code"] + " - " + error["message"]);
-	}
-};
-
-function isAddRelation(obj, relationName){
+function willAddRelation(obj, relationName){
 	var jsonSnippet = '"' + relationName + '":{"__op":"AddRelation"';
-	console.log(jsonSnippet);
-	console.log(JSON.stringify(obj).contains(jsonSnippet));
+	return JSON.stringify(obj).contains(jsonSnippet);
+}
+
+function willRemoveRelation(obj, relationName){
+	var jsonSnippet = '"' + relationName + '":{"__op":"RemoveRelation"';
 	return JSON.stringify(obj).contains(jsonSnippet);
 }
 
 Parse.Cloud.beforeSave("AlienWord", function(req, res) {
 	var word = req.object;
-	var users = word.get("users");
-	if(isAddRelation(word, "users")){
-		console.log("USERS INCREMENT");
+	if(willAddRelation(word, "users")){
 		word.increment("usersCount");
+	}
+	res.success();
+});
+
+Parse.Cloud.beforeDelete("AlienWord", function(req, res) {
+	var word = req.object;
+	if(willRemoveRelation(word, "users")){
+		word.decrement("usersCount");
 	}
 	res.success();
 });
