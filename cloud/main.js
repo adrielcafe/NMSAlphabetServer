97c3a490e3da22416cff9f0726154f1dc9d65afc
Adriel Café <me@adriel.cafe>
2016-05-03 13:34:54 -0300

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -4,8 +4,7 @@ var wordTranslationClass = Parse.Object.extend("AlienWordTranslation");
 // AlienWord
 Parse.Cloud.beforeSave("AlienWord", function(req, res) {
 	var word = req.object;
-	console.log("BEFORESAVE1: " + word);
-	console.log("BEFORESAVE2: " + word.get("users")._op);
+	console.log("BEFORESAVE: " + JSON.stringify(word));
 	word.set("word", word.get("word").toUpperCase())
 	if(willAddRelation(word, "users")){
 		word.increment("usersCount");
