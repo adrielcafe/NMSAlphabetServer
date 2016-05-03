61bf2e99fe935cd3bde54b60e5d66343dbfc6b59
Adriel Café <me@adriel.cafe>
2016-05-03 11:43:04 -0300

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,18 +1,25 @@
 var wordClass = Parse.Object.extend("AlienWord");
 var wordTranslationClass = Parse.Object.extend("AlienWordTranslation");
 
+String.prototype.contains = function(it) { return this.indexOf(it) != -1; };
+
 function printError(tag, error){
 	if(error){
 		console.error("ERROR " + tag + ":" + error["code"] + " - " + error["message"]);
 	}
 };
 
+function isAddRelation(obj){
+	console.log(JSON.stringify(obj));
+	console.log(JSON.stringify(obj).contains("AddRelation")+"");
+	return JSON.stringify(obj).contains("AddRelation");
+}
+
 Parse.Cloud.beforeSave("AlienWord", function(req, res) {
 	var word = req.object;
 	var users = word.get("users");
-	console.log("USERS 1: " + users.__op);
-	if(users.__op === "AddRelation"){
-	console.log("USERS INCREMENT");
+	if(isAddRelation(users)){
+		console.log("USERS INCREMENT");
 		word.increment("usersCount");
 	}
 	res.success();
