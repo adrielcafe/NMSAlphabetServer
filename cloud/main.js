71fce63a0e33c029cf0c807c54d56b54d21d4cff
Adriel Café <me@adriel.cafe>
2016-05-03 11:27:42 -0300

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -8,8 +8,9 @@ function printError(tag, error){
 };
 
 Parse.Cloud.beforeSave("AlienWord", function(req, res) {
-	var tag =  "afterSave_AlienWord";
 	var word = req.object;
+	console.log("WORD 1: " + JSON.stringify(req));
+	console.log("WORD 2: " + JSON.stringify(req.object));
 	word.increment("usersCount");
 	res.success();
 });
