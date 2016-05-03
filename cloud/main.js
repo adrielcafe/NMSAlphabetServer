7a209d3b2dddf9dc6244435701b2750df2435feb
Adriel Café <me@adriel.cafe>
2016-05-03 13:40:56 -0300

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -4,13 +4,11 @@ var wordTranslationClass = Parse.Object.extend("AlienWordTranslation");
 // AlienWord
 Parse.Cloud.beforeSave("AlienWord", function(req, res) {
 	var word = req.object;
-	console.log("BEFORESAVE: " + JSON.stringify(word));
 	word.set("word", word.get("word").toUpperCase())
 	if(willAddRelation(word, "users")){
-		word.increment("usersCount");
+		word.increment("usersCount", 1);
 	} else if(willRemoveRelation(word, "users")){
-		console.log("DECREMENT");
-		word.decrement("usersCount");
+		word.decrement("usersCount", -1);
 	}
 	res.success();
 });
@@ -21,9 +19,9 @@ Parse.Cloud.beforeSave("AlienWordTranslation", function(req, res) {
 	wordTranslation.set("translation", wordTranslation.get("translation").toUpperCase())
 	wordTranslation.set("language", wordTranslation.get("language").toLowerCase())
 	if(willAddRelation(wordTranslation, "users")){
-		wordTranslation.increment("usersCount");
+		wordTranslation.increment("usersCount", 1);
 	} else if(willRemoveRelation(wordTranslation, "users")){
-		wordTranslation.decrement("usersCount");
+		wordTranslation.decrement("usersCount", -1);
 	}
 	res.success();
 });
