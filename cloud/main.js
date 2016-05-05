456b0bb4a18e2bb6005cc060dd1a09a444ec0ee4
Adriel Café <me@adriel.cafe>
2016-05-04 21:58:51 -0300

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -3,14 +3,31 @@ var wordTranslationClass = Parse.Object.extend("AlienWordTranslation");
 
 // AlienWord
 Parse.Cloud.beforeSave("AlienWord", function(req, res) {
-	var word = req.object;
-	word.set("word", word.get("word").toUpperCase())
+	var newWord = req.object;
+	console.log("WORD STR: "+newWord.get("word"));
+	var query = new Parse.Query(wordClass);
+	query.equalTo("race", newWord.get("race"));
+	query.equalTo("word", newWord.get("word"));
+	query.find({
+		success: function(word) {
+			console.log("WORD: "+word);
+			if(word){
+				console.log("OK");
+			} else {
+				console.log("NOPE");
+			}
+			res.success();
+		},
+		error: function(error) {
+			alert("Error: " + error.code + " " + error.message);
+		}
+	});
+	/*word.set("word", word.get("word").toUpperCase())
 	if(willAddRelation(word, "users")){
 		word.increment("usersCount", 1);
 	} else if(willRemoveRelation(word, "users")){
 		word.increment("usersCount", -1);
-	}
-	res.success();
+	}*/
 });
 
 // AlienWordTranslation
